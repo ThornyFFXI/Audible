857c7d138f7c980e6dbf56513f64d7a538d14d82
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2024-08-12 14:34:43 -0400

    Add initial code

---
+++ triggers/mobreadies.lua
@@ -0,0 +1,31 @@
+--[[
+    This triggers when a mob begins casting a spell or readies a weaponskill or job ability.
+    If any of the listed abilities are present, the matching sound will be played.
+]]--
+
+return {
+    --Aita
+    ["Fulminous Smash"] = "Fulminous Smash.wav",
+    ["Flaming Kick"]= "Flaming Kick.wav",
+    ["Icy Grasp"] = "Icy Grasp.wav",
+    ["Flashflood"] = "Flashflood.wav",
+    ["Eroding Flesh"] = "Eroding Flesh.wav",
+    ["Vivisection"] = "Vivisection.wav",
+
+    --Aminon
+    ["Demonfire"] = "Demonfire.wav",
+    ["Frozen Blood"] = "Frozen Bloodh.wav", --Typo?
+    ["Blast of Reticence"] = "Blast of Reticence.wav",
+    ["Ensepulcher"] = "Ensepulcher.wav",
+    ["Ceaseless Surge"] = "Ceaseless Surge.wav",
+    ["Torrential Pain"] = "Torrential Pain.wav",
+
+    --Dhartok
+    ["Cesspool"] = "Cesspool.wav",
+
+    --Gartell
+    ["Shrieking Gale"] = "Shrieking Gale.wav",
+    ["Undulating Shockwave"] = "Undulating Shockwave.wav",
+
+    ['Dread Spikes'] = 'Mob Dread Spikes.wav',
+};
